8b75bfae6d33eed36a901f2832fe85042342edad
Gavin Francis <gav@eh7.co.uk>
2024-11-13 11:02:03 +0000

    add libp2p-webrtc-guide as not submodule

---
+++ libp2p-webrtc-guide/src/node.js
@@ -0,0 +1,147 @@
+// @ts-check
+import { createLibp2p } from 'libp2p'
+import { identify } from '@libp2p/identify'
+import { noise } from '@chainsafe/libp2p-noise'
+import { yamux } from '@chainsafe/libp2p-yamux'
+import { multiaddr } from '@multiformats/multiaddr'
+import { gossipsub } from '@chainsafe/libp2p-gossipsub'
+import { webSockets } from '@libp2p/websockets'
+import { webTransport } from '@libp2p/webtransport'
+import { webRTC, webRTCDirect } from '@libp2p/webrtc'
+import { circuitRelayTransport, circuitRelayServer } from '@libp2p/circuit-relay-v2'
+import { enable, disable } from '@libp2p/logger'
+import { pubsubPeerDiscovery } from '@libp2p/pubsub-peer-discovery'
+import { PUBSUB_PEER_DISCOVERY } from './constants.js'
+import { update, getPeerTypes, getAddresses, getPeerDetails } from './utils.js'
+import { bootstrap } from '@libp2p/bootstrap'
+import * as filters from '@libp2p/websockets/filters'
+
+const App = async () => {
+  const libp2p = await createLibp2p({
+    addresses: {
+      listen: [
+        // 👇 Listen for webRTC connection
+        '/webrtc',
+      ],
+    },
+    transports: [
+      webSockets({
+        // Allow all WebSocket connections inclusing without TLS
+        filter: filters.all,
+      }),
+      webTransport(),
+      webRTC(),
+      // 👇 Required to create circuit relay reservations in order to hole punch browser-to-browser WebRTC connections
+      circuitRelayTransport({
+        discoverRelays: 1,
+      }),
+    ],
+    connectionEncryption: [noise()],
+    streamMuxers: [yamux()],
+    connectionGater: {
+      // Allow private addresses for local testing
+      denyDialMultiaddr: async () => false,
+    },
+    peerDiscovery: [
+      bootstrap({
+        list: ['/ip4/127.0.0.1/tcp/9001/ws/p2p/12D3KooWJynQCnGnjPq1EqeysY4rNm2pwwEApvwxwmFdZWs2DQLD'],
+      }),
+      pubsubPeerDiscovery({
+        interval: 10_000,
+        topics: [PUBSUB_PEER_DISCOVERY],
+      }),
+    ],
+    services: {
+      pubsub: gossipsub(),
+      identify: identify(),
+    },
+  })
+
+  globalThis.libp2p = libp2p
+
+  libp2p.addEventListener('peer:connect', (event) => {
+     console.log('connect:', event)
+  })
+
+  libp2p.addEventListener('peer:disconnect', (event) => {
+     console.log('disconnect:', event)
+  })
+
+  setInterval(() => {
+    console.log('DOM.nodePeerCount()', libp2p.getConnections().length)
+    console.log("libp2p.getConnections::", libp2p
+      .getConnections()
+      .map((conn) => conn.remoteAddr)
+    )
+//    console.log('DOM.nodeAddress', libp2p.getMultiaddrs())
+    console.log('DOM.nodeAddressCount()', libp2p.getMultiaddrs().length)
+
+//    console.log('DOM.nodeAddress', libp2p.getMultiaddrs())
+    console.log("Peers :: ", libp2p
+      .getPeers()
+      .length
+    )
+
+    console.log("PeerId :: ", libp2p.peerId.toString())
+
+//    console.log('DOM.nodePeerTypes()', getPeerTypes(libp2p))
+//    console.log('DOM.nodeAddressCount()', libp2p.getMultiaddrs().length)
+//    console.log('DOM.nodeAddresses()', getAddresses(libp2p))
+//    console.log('DOM.nodePeerDetails()', getPeerDetails(libp2p))
+  }, 5000)
+
+/*
+  const DOM = {
+    nodePeerId: () => document.getElementById('output-node-peer-id'),
+    nodeStatus: () => document.getElementById('output-node-status'),
+    nodePeerCount: () => document.getElementById('output-peer-count'),
+    nodePeerTypes: () => document.getElementById('output-peer-types'),
+    nodePeerDetails: () => document.getElementById('output-peer-details'),
+    nodeAddressCount: () => document.getElementById('output-address-count'),
+    nodeAddresses: () => document.getElementById('output-addresses'),
+
+    inputMultiaddr: () => document.getElementById('input-multiaddr'),
+    connectButton: () => document.getElementById('button-connect'),
+    loggingButtonEnable: () => document.getElementById('button-logging-enable'),
+    loggingButtonDisable: () => document.getElementById('button-logging-disable'),
+    outputQuery: () => document.getElementById('output'),
+  }
+
+  update(DOM.nodePeerId(), libp2p.peerId.toString())
+  update(DOM.nodeStatus(), 'Online')
+
+  libp2p.addEventListener('peer:connect', (event) => {})
+  libp2p.addEventListener('peer:disconnect', (event) => {})
+
+  setInterval(() => {
+    update(DOM.nodePeerCount(), libp2p.getConnections().length)
+    update(DOM.nodePeerTypes(), getPeerTypes(libp2p))
+    update(DOM.nodeAddressCount(), libp2p.getMultiaddrs().length)
+    update(DOM.nodeAddresses(), getAddresses(libp2p))
+    update(DOM.nodePeerDetails(), getPeerDetails(libp2p))
+  }, 1000)
+
+  DOM.loggingButtonEnable().onclick = (e) => {
+    enable('*,*:debug')
+  }
+  DOM.loggingButtonDisable().onclick = (e) => {
+    disable()
+  }
+
+  DOM.connectButton().onclick = async (e) => {
+    e.preventDefault()
+    let maddr = multiaddr(DOM.inputMultiaddr().value)
+
+    console.log(maddr)
+    try {
+      await libp2p.dial(maddr)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+*/
+}
+
+App().catch((err) => {
+  console.error(err) // eslint-disable-line no-console
+})
